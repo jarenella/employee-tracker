eae77d2e881a5b88352d5ad80a7760693e513df7
jarenella <jamesa122333@gmail.com>
2022-10-21 11:23:55 -0700

    put the db connection code in its own connection.js file and linked it to other necessary files. Finished and tested addDepartmentInquire.js function

--- app.js
+++ app.js
@@ -2,17 +2,8 @@ const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const startingPrompt = require("./lib/startingPrompt");
 require('dotenv').config();
-
 //connect to the database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: "employe_information_db"
-    },
-    console.log("Successfully connected to the database")
-)
+const db = require("./lib/connection");
 
 //run the initial prompt
 startingPrompt();
