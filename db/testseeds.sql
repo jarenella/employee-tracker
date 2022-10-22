e8a6afd338726f1a7dde8cff9f3e949b06217f73
jarenella <jamesa122333@gmail.com>
2022-10-21 18:48:45 -0700

    worked on addEmployeeInquire.js file function. It now queries the database to find roles, and uses those roles to make a list asking the user which role their new employee is. It also queries the list of employees for those who are listed as managers and adds them to an array that is used to list the managers to the user when asking them which manager is in charge of the new employee. The database then combs through managers by name to find their ID, so the managers ID can be added to the employees manager_id column

--- db/testseeds.sql
+++ db/testseeds.sql
@@ -8,7 +8,7 @@ VALUES ("Salesman", 50000, 1),
 ("Accountant", 75000, 3),
 ("Manager", 90000, 2);
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Manny", "McManager", 3, NULL),
-(2, "Jane", "Doe", 2, 1),
-(3, "Saley", "McSalesman", 1, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Manny", "McManager", 3, NULL),
+("Jane", "Doe", 2, 1),
+("Saley", "McSalesman", 1, 1);
